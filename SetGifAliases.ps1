ed27ddfe3b9fa16409fd7e270318523d828ea7a0
Scott Hanselman <scott@hanselman.com>
2019-06-28 15:53:13 -0700

    closes #4 very poorly

--- SetGifAliases.ps1
+++ SetGifAliases.ps1
@@ -3,14 +3,18 @@
 Function DotNetAlias {
     dotnet build
     if ($?) {
-        d:\github\TerminalAttractMode\SetMoodGif.ps1 "PowerShell Core" "D:\Dropbox\Reference\Animated Gifs\chrispratt.gif"                            
-        Start-Sleep 1.5
-        d:\github\TerminalAttractMode\SetMoodGif.ps1 "PowerShell Core" "D:\Dropbox\Reference\Animated Gifs\4003cn5.gif"                            
+        Start-job -ScriptBlock {
+            d:\github\TerminalAttractMode\SetMoodGif.ps1 "PowerShell Core" "D:\Dropbox\Reference\Animated Gifs\chrispratt.gif"                            
+            Start-Sleep 1.5
+            d:\github\TerminalAttractMode\SetMoodGif.ps1 "PowerShell Core" "D:\Dropbox\Reference\Animated Gifs\4003cn5.gif"                            
+        } | Out-Null
     }
     else {
-        d:\github\TerminalAttractMode\SetMoodGif.ps1 "PowerShell Core" "D:\Dropbox\Reference\Animated Gifs\idk-girl.gif"                            
-        Start-Sleep 1.5
-        d:\github\TerminalAttractMode\SetMoodGif.ps1 "PowerShell Core" "D:\Dropbox\Reference\Animated Gifs\4003cn5.gif"                            
+        Start-job -ScriptBlock {
+            d:\github\TerminalAttractMode\SetMoodGif.ps1 "PowerShell Core" "D:\Dropbox\Reference\Animated Gifs\idk-girl.gif"                            
+            Start-Sleep 1.5
+            d:\github\TerminalAttractMode\SetMoodGif.ps1 "PowerShell Core" "D:\Dropbox\Reference\Animated Gifs\4003cn5.gif"                            
+        } | Out-Null
 
     }
 }
